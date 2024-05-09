323b95a7c5fb3df30bbe1c9ff7f46feca4c50410
Ninad <ninaddhikale@gmail.com>
2024-05-09 14:10:28 +0530

    First Commit

---
+++ PartA/file3.c
@@ -0,0 +1,29 @@
+void checkEvenOdd(int N) 
+{ 
+    // Find remainder 
+    int r = N % 2; 
+  
+    // Condition for even 
+    if (r == 0)  
+    { 
+        printf("Even"); 
+    } 
+  
+    // Otherwise 
+    else 
+    { 
+        printf("Odd"); 
+    } 
+} 
+  
+// Driver Code 
+int main() 
+{ 
+    // Given number N 
+    int N = 101; 
+  
+    // Function Call 
+    checkEvenOdd(N); 
+  
+    return 0; 
+}
